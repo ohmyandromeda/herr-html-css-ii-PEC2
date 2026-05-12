8b40b984086f732f7df961e52a5de488cae5c621
Andrea Esteve Herranz <aesteveh@uoc.edu>
2026-05-12 22:41:55 +0200

    Ajustes para que sea el enunciado 100%

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -16,3 +16,23 @@
 })();
 
 import "bootstrap";
+
+import { animate, splitText, stagger } from "animejs";
+
+const titleTarget = ".poster-title";
+
+const split = splitText(titleTarget, {
+  lines: true,
+  words: true,
+  chars: true,
+});
+
+split.addEffect(({ chars }) => {
+  return animate(chars, {
+    opacity: [0, 1],
+    y: [20, 0],
+    duration: 600,
+    delay: stagger(30),
+    ease: "out(4)",
+  });
+});
